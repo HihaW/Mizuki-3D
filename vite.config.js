20621b9a13d2ebfcf3d32d214f90c63768951db3
HihaW <138550610+HihaW@users.noreply.github.com>
2026-08-06 18:46:04 +0700

    Fix base path in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/Mizuki-3D', 
+  base: '/Mizuki-3D/', 
   build: {
     target: 'es2020',
     chunkSizeWarningLimit: 1200
